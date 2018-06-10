5d0d4edf626bd9fd13b5860167ef215cf0fd5b16
John <momogoing@163.com>
2018-06-10 23:58:19 +0800

    update-23:58

--- bootz-usercenter/bootz-usercenter-web/src/main/java/top/bootz/user/config/app/MongoConfig.java
+++ bootz-usercenter/bootz-usercenter-web/src/main/java/top/bootz/user/config/app/MongoConfig.java
@@ -4,99 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.auditing.CurrentDateTimeProvider;
 import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoExceptionTranslator;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.core.mapping.event.LoggingEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 
 import top.bootz.commons.converter.DateToLocalDateConverter;
 import top.bootz.commons.converter.DateToLocalDateTimeConverter;
 import top.bootz.commons.converter.DateToTimestampConverter;
 import top.bootz.commons.converter.LocalDateTimeToDateConverter;
 import top.bootz.commons.converter.LocalDateToDateConverter;
-import top.bootz.user.properties.MongoConfigProperties;
 
 @Configuration
 @EnableMongoRepositories(basePackages = { "top.bootz.user.biz.persist.mongo" })
 @EnableMongoAuditing(auditorAwareRef = "mongoAuditorProvider", dateTimeProviderRef = "mongoDateTimeProvider", modifyOnCreate = false)
-public class MongoConfig extends AbstractMongoConfiguration {
-
-	@Autowired
-	private MongoConfigProperties configProperties;
-
-	// 覆盖默认的MongoDbFactory
-	@Bean
-	@Override
-	public MongoDbFactory mongoDbFactory() {
-		return new SimpleMongoDbFactory(mongoClient(), configProperties.getDatabase());
-	}
-
-	@Override
-	protected String getDatabaseName() {
-		return configProperties.getDbname();
-	}
-
-	@Bean
-	@Override
-	public MongoClient mongoClient() {
-		// 客户端配置（连接数、副本集群验证）
-		MongoClientOptions.Builder builder = new MongoClientOptions.Builder();
-		builder.connectionsPerHost(configProperties.getConnectionsPerHost());
-		builder.minConnectionsPerHost(configProperties.getMinConnectionsPerHost());
-		if (configProperties.getReplicaSet() != null) {
-			builder.requiredReplicaSetName(configProperties.getReplicaSet());
-		}
-		MongoClientOptions mongoClientOptions = builder.build();
-
-		// MongoDB地址列表
-		List<ServerAddress> serverAddresses = new ArrayList<>();
-		for (String host : configProperties.getHosts()) {
-			Integer index = configProperties.getHosts().indexOf(host);
-			Integer port = configProperties.getPorts().get(index);
-
-			ServerAddress serverAddress = new ServerAddress(host, port);
-			serverAddresses.add(serverAddress);
-		}
-
-		// 连接认证
-		MongoCredential mongoCredential = null;
-		if (configProperties.getUsername() != null) {
-			mongoCredential = MongoCredential.createScramSha1Credential(
-					configProperties.getUsername(), configProperties.getAuthenticationDatabase() != null
-							? configProperties.getAuthenticationDatabase() : configProperties.getDatabase(),
-					configProperties.getPassword().toCharArray());
-		}
-
-		// 创建客户端和Factory
-		MongoClient mongoClient = null;
-		if (mongoCredential != null) {
-			mongoClient = new MongoClient(serverAddresses, mongoCredential, mongoClientOptions);
-		} else {
-			mongoClient = new MongoClient(serverAddresses);
-		}
-		return mongoClient;
-	}
+public class MongoConfig {
 
 	/**
 	 * 自定义转换器，处理date到timestamp的转换问题
 	 */
-	@Override
+	@Bean
 	public MongoCustomConversions customConversions() {
 		List<Converter<?, ?>> converters = new ArrayList<>();
 		converters.add(DateToTimestampConverter.INSTANCE);
